3a97f3676defda81f462c6be89f8e0e1b187142c
PeaceTobi <peaceshalom9@gmail.com>
2023-02-28 16:33:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+ 
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Navbar  from './components/Navbar';
+import Hero from './components/Hero';
+import Testimonial from './components/Testimonial';
+import Contact from './components/Contact';
+import Footer from  './components/Footer';
+// import { ColorModeSwitcher } from './ColorModeSwitcher';
+// import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Navbar />
+      <Hero />
+      <Testimonial />
+      <Contact />
+      <Footer  />
     </ChakraProvider>
   );
 }
